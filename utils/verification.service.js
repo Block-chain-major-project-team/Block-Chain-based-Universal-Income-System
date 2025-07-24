17f021021de37c1ec2b7a851579beb59f82ed45e
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-07-24 14:40:19 +0530

    created verification service in utils "

---
+++ utils/verification.service.js
@@ -0,0 +1,21 @@
+// utils/verification.service.js
+
+const verifyKycMock = (filename) => {
+    const name = filename.toLowerCase();
+
+    // Auto-approve if file includes "valid"
+    if (name.includes("adhar") || name.includes("pan")) {
+        return "Approved";
+    }
+
+    // Auto-reject if file includes "fake" or "invalid"
+    if (name.includes("fake") || name.includes("invalid")) {
+        return "rejected";
+    }
+
+    // 🔄 Otherwise, randomly approve or reject (simulating AI or OCR decision)
+    const decision = Math.random() > 0.5 ? "approved" : "rejected";
+    return decision;
+};
+
+module.exports = verifyKycMock;
