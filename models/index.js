00f25965e60ff2645ef0f0bd2865a51dbddefeb9
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-07-24 14:38:34 +0530

    updated index.js for model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const CONFIG = require("../config/config");
 const logger = require("../utils/logger.service");
 
@@ -8,36 +9,42 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const db = {};
 
-let sequelize;
 const sequelizeOptions = {
-    host: CONFIG.db_host,
-    port: CONFIG.db_port,
-    dialect: CONFIG.db_dialect || "postgres", // Ensure a default dialect if not supplied in config
-    pool: { max: 20, min: 0, acquire: 30000, idle: 10000 },
-    logging: msg => logger.debug(msg),
+  host: CONFIG.db_host,
+  port: CONFIG.db_port,
+  dialect: CONFIG.db_dialect || "postgres",
+  pool: {
+    max: 20,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  logging: msg => logger.debug(msg),
 };
 
-if (CONFIG.db_usePassword === "true") {
-    sequelize = new Sequelize(CONFIG.db_name, CONFIG.db_user, CONFIG.db_password, sequelizeOptions);
-} else {
-    sequelize = new Sequelize(CONFIG.db_name, CONFIG.db_user, null, sequelizeOptions);
-}
+// ✅ Always pass string or null based on boolean flag
+const dbPassword = CONFIG.db_usePassword ? String(CONFIG.db_password || "") : null;
 
+const sequelize = new Sequelize(
+  CONFIG.db_name,
+  CONFIG.db_user,
+  dbPassword,
+  sequelizeOptions
+);
+
+// Load all models in this folder
 fs.readdirSync(__dirname)
-    .filter(file => {
-        return (
-            file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-        );
-    })
-    .forEach(file => {
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-        db[model.name] = model;
-    });
+  .filter(file => file.indexOf(".") !== 0 && file !== basename && file.endsWith(".js"))
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
+// Apply associations
 Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-        db[modelName].associate(db);
-    }
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
