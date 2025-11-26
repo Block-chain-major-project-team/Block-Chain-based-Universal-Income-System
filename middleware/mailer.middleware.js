9c988fcc22fe3ff276cb9f567a982b43d8c38595
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-11-26 08:45:29 +0530

    update

--- middleware/mailer.middleware.js
+++ middleware/mailer.middleware.js
@@ -1,20 +1,23 @@
+"use strict";
+
 const nodemailer = require("nodemailer");
 const CONFIG = require("../config/config");
 
+// Use SendGrid SMTP
 const transporter = nodemailer.createTransport({
-  host: CONFIG.mailHost,
-  port: CONFIG.mailPort,
-  secure: CONFIG.mailSecure,
+  host: CONFIG.mailHost,      // smtp.sendgrid.net
+  port: CONFIG.mailPort,      // 587 for TLS, 465 for SSL
+  secure: CONFIG.mailSecure,  // false for TLS (STARTTLS)
   auth: {
-    user: CONFIG.mailUser,
-    pass: CONFIG.mailPassword
+    user: CONFIG.mailUser,    // must be 'apikey'
+    pass: CONFIG.mailPassword // your SendGrid API key
   }
 });
 
-// Generic mail sender
+// Generic mail sender (function name unchanged)
 const sendMail = async (to, subject, html) => {
   const mailOptions = {
-    from: `"BLOCKCHAINUBI TEAM" <${CONFIG.mailUser}>`, // 👈 Custom sender name
+    from: `"BLOCKCHAINUBI TEAM" <${CONFIG.mailUser}>`, // your sender name
     to,
     subject,
     html
